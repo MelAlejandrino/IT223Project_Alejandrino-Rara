37bfdd1b3689f879364de83080a4d0d4c3f17815
MelAlejandrino <alejandrinomel02@gmail.com>
2023-05-02 17:03:09 +0800

    dropdown navigation

--- src/script.js
+++ src/script.js
@@ -1,14 +1,4 @@
 // atong javascript file
-const blob = document.getElementById("skull");
-
-window.onpointermove = event => { 
-  const { clientX, clientY } = event;
-  
-  blob.animate({
-    left: `${clientX}px`,
-    top: `${clientY}px`
-  }, { duration: 1000, fill: "forwards" });
-}
 
 // icon
 // name
@@ -27,21 +17,81 @@ if (window.matchMedia("(max-width: 640px)").matches) {
     namePC.removeAttribute('hidden', '');
   }
 
-//   nav - tab
+  function isHeader(atHeader){
+    return atHeader;
+  }
+  // nav-tab
 const navTab = document.querySelector('.nav-holder');
 const navIcon = document.getElementById('menu-icon');
 const header = document.getElementById('header-tab');
+const sections = document.querySelectorAll('section');
+const headerpage = document.querySelector('.headpage');
+const navigation = document.querySelector('.nav-tab');
+let atHeader;
+const observer = new IntersectionObserver(function(entries, observer){
+
+  entries.forEach(entry => {
+    const visibility = navTab.getAttribute("data-visible");
+    if(entry.isIntersecting){
+      atHeader = true;
+      navigation.classList.remove('navchange');
+      if(atHeader === true){
+        if(visibility === "true"){
+          navIcon.src = 'src/assets/menublack.png';
+        }
+        if(visibility === "false"){
+          navIcon.src = 'src/assets/menu.png';
+        }
+      }
+      
+    }
+    else{
+      atHeader = false;
+      navigation.classList.add('navchange');
+      if(atHeader === false){
+        console.log(atHeader)
+      }
+      navIcon.src = 'src/assets/menublack.png';
+    }
+  })
+});
+
+observer.observe(headerpage);
+
+
+
 navIcon.addEventListener('click', () => {
-  console.log('working')
+  const headerStatus = isHeader(atHeader);
   const visibility = navTab.getAttribute("data-visible");
   if (visibility === "false"){
     navTab.setAttribute("data-visible", true);
     header.setAttribute("aria-expanded", true);
-    navIcon.src = 'src/assets/menublack.png';
+    if (headerStatus){
+      navIcon.src = 'src/assets/menublack.png';
+    }
+    if (!headerStatus){
+      navIcon.src = 'src/assets/menublack.png';
+    }
+    sections.forEach((section) => {
+      section.classList.add('blursection');
+    });
   }
   else{
     navTab.setAttribute("data-visible", false);
     header.setAttribute("aria-expanded", false);
-    navIcon.src = 'src/assets/menu.png';
+    if (headerStatus === true){
+      navIcon.src = 'src/assets/menu.png';
+    }
+    if (headerStatus === false){
+      navIcon.src = 'src/assets/menublack.png';
+    }
+    sections.forEach((section) => {
+      section.classList.remove('blursection');
+    });
   }
 });
+
+
+
+
+
