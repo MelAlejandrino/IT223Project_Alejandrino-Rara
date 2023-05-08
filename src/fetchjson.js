d4e3a8d22836a46a4983e70d0aedf3ae9245d9f6
MelAlejandrino <alejandrinomel02@gmail.com>
2023-05-08 21:18:07 +0800

    filter categories

--- src/fetchjson.js
+++ src/fetchjson.js
@@ -1,10 +1,60 @@
 const container = document.querySelector(".prodContainer");
-fetch("src/products.json")
-  .then((response) => response.json())
-  .then((data) => {
-    data.forEach((item) => {
-      container.innerHTML += `
-      <div class="card">
+const radioButtons = document.getElementsByName('filtCategories');
+const labels = document.querySelectorAll('label');
+
+// Add event listeners to labels
+labels.forEach((label) => {
+  label.addEventListener('click', function() {
+    const radioButton = document.getElementById(this.getAttribute('for'));
+    radioButton.checked = true;
+
+    // Trigger 'change' event on the radio button
+    const event = new Event('change');
+    radioButton.dispatchEvent(event);
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  fetchProducts();
+});
+
+for (let i = 0; i < radioButtons.length; i++) {
+  radioButtons[i].addEventListener('change', function() {
+    container.innerHTML = ''; // Clear existing results before fetching and displaying products
+
+    if (this.value === "allCateg") {
+      fetchProducts();
+    }
+
+    if (this.value === "disCateg") {
+      fetch("src/products.json")
+        .then((response) => response.json())
+        .then((data) => {
+          data.forEach((item) => {
+            if (item.disc === "YES") {
+              displayProduct(item);
+            }
+          });
+        })
+        .catch((error) => console.error(error));
+    }
+  });
+}
+
+function fetchProducts() {
+  fetch("src/products.json")
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((item) => {
+        displayProduct(item);
+      });
+    })
+    .catch((error) => console.error(error));
+}
+
+function displayProduct(item) {
+  container.innerHTML += `
+    <div class="card">
       <div class="card-img">
         <picture>
           <img src="${item.src}" alt="" />
@@ -21,7 +71,5 @@ fetch("src/products.json")
         </div>
       </div>
     </div>
-        `;
-    });
-  })
-  .catch((error) => console.error(error));
+  `;
+}
