b658c4f26670a27dfd586c3019d5930f3e19b628
MelAlejandrino <alejandrinomel02@gmail.com>
2023-06-28 00:46:56 +0800

    lastlast

--- src/script.js
+++ src/script.js
@@ -1,16 +1,18 @@
 // atong javascript file
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-      if(entry.isIntersecting){
-          entry.target.classList.add('show');
-      } else {
-          entry.target.classList.remove('show');
-      }
-  });
-});
+// const observer = new IntersectionObserver((entries) => {
+//   entries.forEach((entry) => {
+//       if(entry.isIntersecting){
+//           entry.target.classList.add('show');
+//           console.log('i am nav')
+//       } else {
+//           entry.target.classList.remove('show');
+//       }
+//   });
+// });
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+const navpctab = document.querySelector('.navpctab');
 // icon
 // name
 
@@ -18,15 +20,19 @@ const namePC = document.getElementById("icon");
 window.addEventListener("resize", () => {
   if (window.matchMedia("(max-width: 640px)").matches) {
     namePC.setAttribute("hidden", "");
+    navpctab.classList.add('hidden');
   } else {
     namePC.removeAttribute("hidden", "");
+    navpctab.classList.remove('hidden');
   }
 });
 
 if (window.matchMedia("(max-width: 640px)").matches) {
   namePC.setAttribute("hidden", "");
+      navpctab.classList.add('hidden');
 } else {
   namePC.removeAttribute("hidden", "");
+      navpctab.classList.remove('hidden');
 }
 
 const headerspage = document.querySelector('.headpage')
